0eb8a213afeab651d272181eb052505b4c4251b1
Olubukola Ayodele <116734274+ayodelebukola@users.noreply.github.com>
2023-02-26 22:11:16 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let InstagramPost = {}
+InstagramPost.handleOfAuthor = 'Foodnetworkng';
+InstagramPost.content = 'Food'
+InstagramPost.link = 'https://www.instagram.com/p/CouUHgWDjIy/';
+InstagramPost.numberOfViews =2000000;
+InstagramPost.numberOfLikes = 89000;
+console.log(InstagramPost)
+
